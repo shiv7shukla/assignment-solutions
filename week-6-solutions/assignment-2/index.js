296c003a76fd7b7a0de1e884b1bfbac6cfc0cbd5
Shiv Shukla <shivshukla00514@gmail.com>
2025-07-10 13:43:51 +0530

    signup, singnin endpoints done

---
+++ week-6-solutions/assignment-2/index.js
@@ -0,0 +1,42 @@
+const express=require("express");
+const app=express();
+
+const jwt=require('jsonwebtoken');
+const JWT_SECRET="anything";
+
+const{z}=require("zod");
+
+const users=[],todos=[];
+
+app.use(express());
+
+app.post("/signup",(req,res)=>{
+    const required=z.object({
+        email:z.email().string().min(3).max(100),
+        password:z.string().min(3).max(100),
+        name:z.string().min(3).max(100)
+    })
+    if(!((required.safeParse(req.body)).success))
+        res.json({msg:"incorrect credentials"});
+    else
+    {
+        const username=req.body.username;
+        const password=req.body.password;
+        const name=req.body.name;
+        users.push({name,username,password});
+        res.json({msg:"you are signed up"});
+    }
+})
+
+app.post("/signin",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+    if(users.find(u=>u.username===username&&u.password===password))
+    {
+        const token=jwt.sign({username},JWT_SECRET);
+        res.json({msg:"sign in successfully",token});
+    }
+    else
+        res.json({msg:"invalid credentials"});
+})
+
