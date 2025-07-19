7ce831202e84745acae87b1c8162e76e02769647
Shiv Shukla <shivshukla00514@gmail.com>
2025-07-19 12:13:15 +0530

    schema added and database connected

---
+++ week-8-solutions/db.js
@@ -0,0 +1,45 @@
+const mongoose=require("mongoose");
+async ()=>{
+    try{
+        await mongoose.connect("mongodb+srv://shivs_admin:142005@cluster0.iwlb894.mongodb.net/course-app");}
+    catch{
+        console.log("error in connecting to database");}
+}
+
+
+const Schema=mongoose.Schema;
+const ObjectId=mongoose.ObjectId;
+
+const User=new Schema({
+    email:{type:String,unique:true},
+    password:String,
+    firstName:String,
+    lastName:String
+})
+
+const Course=new Schema({
+    title:String,
+    description:String,
+    price:Number,
+    imageurl:String,
+    creatorId:ObjectId
+})
+
+const Admin=new Schema({
+    email:{type:String,unique:true},
+    password:String,
+    firstName:String,
+    lastName:String
+})
+
+const Purchases=new Schema({
+    courseId:ObjectId,
+    userId:ObjectId
+})
+
+const UserModel=mongoose.model("user",User);
+const AdminModel=mongoose.model("admin",Admin);
+const CourseModel=mongoose.model("course",Course);
+const PurchaseModel=mongoose.model("purchase",Purchases);
+
+module.exports={UserModel,AdminModel,CourseModel,PurchaseModel};
--- week-8-solutions/routes/admin.js
+++ week-8-solutions/routes/admin.js
@@ -1,4 +1,5 @@
 const {Router}=require("express");
+const{AdminModel}=require("../db");
 
 const adminRouter=Router();
 
@@ -8,6 +9,21 @@ adminRouter.post("/signup",(req,res)=>{
 
 //signin endpoint
 adminRouter.post("/signin",(req,res)=>{
+
+})
+
+//course creation endpoint
+adminRouter.post("/course",(req,res)=>{
+    
+})
+
+//change the course endpoint
+adminRouter.put("/course",(req,res)=>{
+
+})
+
+//all the courses created endpoint
+adminRouter.get("/course/bulk",(req,res)=>{
 
 })
 
