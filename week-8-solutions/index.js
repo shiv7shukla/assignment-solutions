8e2ed4132548621af1c2825626f0ab24d33107a1
Shiv Shukla <shivshukla00514@gmail.com>
2025-07-19 09:04:46 +0530

    added skeleton routes

---
+++ week-8-solutions/index.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const app=express();
+
+//signin endpoint
+app.post("/signup",(req,res)=>{
+
+})
+
+//signin endpoint
+app.post("/signin",(req,res)=>{
+
+})
+
+//purchases made by user
+app.get("/user/purchases",(req,res)=>{
+
+})
+
+//course to be purchased by the user
+app.post("/course/purchase",(req,res)=>{
+
+})
+
+//all the courses available
+app.get("/courses",(req,res)=>{
+
+})
+
+app.listen(3000);
